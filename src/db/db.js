fe47d155bae348e1f634d8507aec01007717a4f6
Atif Afsar <atifafsar70@gmail.com>
2025-09-12 21:56:12 +0530

    Remove hardcoded MongoDB URI and use env variable

--- src/db/db.js
+++ src/db/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 function connectDB(){
-    mongoose.connect("mongodb+srv://Nodejs:kplgFAXjetntI3My@cluster1.wkfp8.mongodb.net/restaurantDB")
+    mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(()=>{
         console.log("Database connected")
     })
