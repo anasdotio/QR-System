87892c5692e7cbf5e08599360ae9f8d61ddf9b7b
Anas <anasroyal385@gmail.com>
2025-09-19 20:20:21 +0530

    add utility classes for API error handling and response formatting

---
+++ src/utils/apiError.js
@@ -0,0 +1,14 @@
+class apiError extends Error {
+  constructor(statusCode, message = "Something went wrong", errors = []) {
+    super(message);
+    this.statusCode = statusCode;
+    this.message = message;
+    this.errors = errors;
+    this.success = false;
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+module.exports = apiError;
