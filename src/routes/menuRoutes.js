7bb627763c93f1d47e624834cdc47f5e3a0f6497
Anas <anasroyal385@gmail.com>
2025-09-20 12:11:38 +0530

    remove neccessary space

--- src/routes/menuRoutes.js
+++ src/routes/menuRoutes.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { verifyAdmin } = require("../middleware/auth"); // JWT middleware
+const { verifyAdmin } = require('../middleware/auth'); // JWT middleware
 
-const multer = require("multer");
-const { createMenuItem, getMenu } = require("../controllers/menuController");
+const multer = require('multer');
+const { createMenuItem, getMenu } = require('../controllers/menuController');
 
 const upload = multer({ storage: multer.memoryStorage() });
 
-
 // Protected - only admin can add item
-router.post("/", verifyAdmin, upload.single("itemImage"), createMenuItem);
-router.get("/", getMenu);
+router.post('/', verifyAdmin, upload.single('itemImage'), createMenuItem);
+router.get('/', getMenu);
 
 module.exports = router;
